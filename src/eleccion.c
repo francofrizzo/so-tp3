abc8b31f4e31462fd981892d840357755c31ab82
Iván Pondal <ivan.pondal@gmail.com>
2016-06-16 21:56:22 -0300

    Add ring communication without dead processes

--- src/eleccion.c
+++ src/eleccion.c
@@ -16,13 +16,15 @@ static t_pid siguiente_pid(t_pid pid, int es_ultimo){
 void iniciar_eleccion(t_pid pid, int es_ultimo){
 	MPI_Request req;
 
-	t_pid token[2];
+	t_pid token[4];
 	token[0] = pid;
 	token[1] = pid;
+	token[2] = pid;
+	token[3] = 0;
 	
 	t_pid siguiente = siguiente_pid(pid, es_ultimo);
 
-	MPI_Isend(&token, 2, MPI_PID, siguiente, TAG_ELECCION_TOKEN,
+	MPI_Isend(&token, 4, MPI_PID, siguiente, TAG_ELECCION_TOKEN,
 		MPI_COMM_WORLD, &req);
 
 	double ahora = MPI_Wtime();
@@ -35,31 +37,79 @@ void iniciar_eleccion(t_pid pid, int es_ultimo){
 			&ack_flag, &ack_status);
 		ahora = MPI_Wtime();
 	}
-	printf("pasoeltiempo");
-	// acá estoy seguro de que recibí el ACK
+	printf("[%hd, %hd] (%u -> %u) i:%hd  c:%hd\n", token[0], token[1], pid, siguiente, token[2], token[3]);
 }
 
 void eleccion_lider(t_pid pid, int es_ultimo, unsigned int timeout){
 	static t_status status = NO_LIDER;
 	double ahora = MPI_Wtime();
 	double tiempo_maximo = ahora + timeout;
-	t_pid proximo = siguiente_pid(pid, es_ultimo);
+	double ahora_ack = 0;
+	double tiempo_maximo_ack = 0;
+	int ack_flag = 0;
+
+	t_pid siguiente = siguiente_pid(pid, es_ultimo);
+	t_pid ack_pid;
 
 	int token_flag = 0;
 	MPI_Status token_status;
+	MPI_Status ack_status;
 
 	MPI_Request req;
-	t_pid token[2];
+	t_pid token[4];
 
+	// Repito hasta qye haya un líder
 	while (ahora < tiempo_maximo){
+		// Reviso si llegó un mensaje nuevo
 		MPI_Iprobe(MPI_ANY_SOURCE, TAG_ELECCION_TOKEN, MPI_COMM_WORLD,
-			&token_flag, &token_status);
+		           &token_flag, &token_status);
 
+		// Si llegó un mensaje nuevo lo cargo en token
 		if (token_flag) {
-			MPI_Irecv(&token, 2, MPI_PID, MPI_ANY_SOURCE, TAG_ELECCION_TOKEN,
-				MPI_COMM_WORLD, &req);
+			// Leo el mensaje recibido
+			MPI_Irecv(&token, 4, MPI_PID, MPI_ANY_SOURCE, TAG_ELECCION_TOKEN,
+			          MPI_COMM_WORLD, &req);
 
-			printf("%hd %hd\n", token[0], token[1]);
+			// Tomo el pid del emisor y le envío ACK
+			ack_pid = token_status.MPI_SOURCE;
+			MPI_Isend(&token_flag, 1, MPI_PID, ack_pid, TAG_ELECCION_ACK,
+			          MPI_COMM_WORLD, &req);
+
+			// Si llegó un mensaje en el cual soy el inciador
+			if (token[0] == pid) {
+				// Si yo sigo siendo el candidato
+				if (token[1] == pid) {
+					// Soy líder
+					status = LIDER;
+					break;
+				}
+				else {
+					// Reemplazo al inciciador por el candidato actual
+					token[0] = token[1];
+				}
+			}
+			// Si no soy el iniciador pero tengo un pid mayor al del candidato
+			else if(token[0] != pid && token[1] < pid) {
+				// Me denomino como nuevo candidato
+				token[1] = pid;
+			}
+			token[3]++;
+
+			// Envío el nuevo token al próximo
+			MPI_Isend(&token, 4, MPI_PID, siguiente, TAG_ELECCION_TOKEN,
+			          MPI_COMM_WORLD, &req);
+
+			ahora_ack = MPI_Wtime();
+			tiempo_maximo_ack = ahora_ack + ACK_TIMEOUT;
+
+			// Espero el ACK
+			ack_flag = 0;
+			while (! ack_flag && ahora_ack < tiempo_maximo_ack) {
+				MPI_Iprobe(siguiente, TAG_ELECCION_ACK, MPI_COMM_WORLD,
+				           &ack_flag, &ack_status);
+				ahora_ack = MPI_Wtime();
+			}
+			printf("[%hd, %hd] (%u -> %u) i:%hd  c:%hd\n", token[0], token[1], pid, siguiente, token[2], token[3]);
 		}
 
 		/* Actualizo valor de la hora. */
